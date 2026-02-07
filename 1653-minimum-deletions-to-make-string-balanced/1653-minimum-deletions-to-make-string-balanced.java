07eedb83a5e857ce6e9eaa6246848ccecc142f8f
Shyamsundheraaj5 <148530451+Shyamsundheraaj5@users.noreply.github.com>
2026-02-07 22:36:51 +0530

    Time: 19 ms (93.09%), Space: 48 MB (16.28%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,15 +1,15 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int c =0;
-        int deletion=0;
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch == 'b' ) c++;
-            if(ch == 'a' && c>0){
-                c--;
-                deletion++;
+        int countB=0;
+        int deletions = 0;
+
+        for(char ch: s.toCharArray()){
+            if(ch=='b')
+                countB++;   
+            else{
+                deletions = Math.min(deletions+1,countB);
             }
         }
-        return deletion;
+        return deletions;
     }
 }
