3e04e4b9840d6861abe20676e33688763d7f45c1
Shyamsundheraaj5 <148530451+Shyamsundheraaj5@users.noreply.github.com>
2026-02-15 09:15:34 +0530

    Time: 2 ms (68.56%), Space: 42.9 MB (63.32%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -2,17 +2,15 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int a =0;
         int b = 0;
-        int count=0;
         if(s.length() == 0) return true;
         while(a<s.length() && b<t.length()){
             char ch1 = s.charAt(a);
             char ch2 = t.charAt(b);
             if(ch1 == ch2){
                 a++;
-                count++;
             }
             b++;
         }
-        return (count==s.length())? true:false;
+        return (a==s.length())? true:false;
     }
 }
